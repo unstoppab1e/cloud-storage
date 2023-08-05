651d6a271975db64fa5386f5341ffce9ed532be6
unstoppab1e <unstoppab1e@users.noreply.github.com>
2023-08-05 21:41:20 +0900

    basic settings

---
+++ src/files/files.controller.ts
@@ -0,0 +1,47 @@
+import {
+  Controller,
+  Post,
+  UseInterceptors,
+  UploadedFile,
+  ParseFilePipe,
+  MaxFileSizeValidator,
+} from '@nestjs/common';
+import { FilesService } from './files.service';
+import { ApiBody, ApiConsumes, ApiTags } from '@nestjs/swagger';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { fileStorage } from './storage';
+
+@Controller('files')
+@ApiTags('files')
+export class FilesController {
+  constructor(private readonly filesService: FilesService) {}
+
+  @Post()
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: fileStorage,
+    }),
+  )
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        file: {
+          type: 'string',
+          format: 'binary',
+        },
+      },
+    },
+  })
+  create(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [new MaxFileSizeValidator({ maxSize: 1024 * 1024 * 5 })],
+      }),
+    )
+    file: Express.Multer.File,
+  ) {
+    return file;
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as express from 'express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: false });
+  const config = new DocumentBuilder()
+    .setTitle('Cloudinary API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
+  app.enableCors({ credentials: true, origin: true });
+  app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
   await app.listen(3000);
 }
 bootstrap();
